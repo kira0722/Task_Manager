e0779f6e92fd1979697d89baa52d3410feefd61d
kria0722 <samuel_quirama23211@elpoli.edu.co>
2025-06-19 15:01:46 -0500

    subiendo metodos para tipos de eventos

--- src/app/pages/labs/labs.ts
+++ src/app/pages/labs/labs.ts
@@ -13,4 +13,24 @@ export class Labs {
     'Install Angular CLI',
     'Create project'
   ];
+
+  name = 'carlitos'
+  age = 21
+
+  disabled = true
+  img = 'https://wpcdn.web.wsu.edu/cahnrs/uploads/sites/4/cat2-1024x676.jpg'
+
+  person = {
+    name: 'Pedrito',
+    age: 34,
+    avatar: 'https://st3.depositphotos.com/11660552/18285/v/450/depositphotos_182857044-stock-illustration-vector-illustration-of-a-crow.jpg'
+  }
+
+  clickHandler(){
+    alert('hi')
+  }
+
+  changeHandler(event: Event){
+    console.log(event);
+  }
 }
