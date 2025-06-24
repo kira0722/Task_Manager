1f6531b73f85330a26135962160393189e26c526
kria0722 <samuel_quirama23211@elpoli.edu.co>
2025-06-24 12:14:10 -0500

    subiendo mutabilidad de las variables, y formularios dinamicos

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,11 +1,14 @@
+import { Component, computed, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Component, signal } from '@angular/core';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+
 import { Task } from '../../Models/task.model';
+
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
@@ -14,45 +17,106 @@ export class Home {
     {
       id: Date.now(),
       title: 'Crear proyecto',
-      completed: false
+      completed: true
     },
     {
       id: Date.now(),
       title: 'Crear componenentes',
       completed: false
-    },
+    }
   ]);
+  filter = signal<'all' | 'pending' | 'completed'>('all');
+  tasksByFilter = computed(() => {
+    const filter = this.filter();
+    const tasks = this.tasks();
+    if (filter === 'pending') {
+      return tasks.filter(task => !task.completed);
+    }
+    if (filter === 'completed') {
+      return tasks.filter(task => task.completed);
+    }
+    return tasks;
+  })
 
-  changeHandler(event: Event) {
-    const input = event.target as HTMLInputElement;
-    const newTask = input.value;
-    this.addTask(newTask);
+  newTaskCtrl = new FormControl('', {
+    nonNullable: true,
+    validators: [
+      Validators.required,
+    ]
+  });
+
+  changeHandler() {
+    if (this.newTaskCtrl.valid) {
+      const value = this.newTaskCtrl.value.trim();
+      if (value !== '') {
+        this.addTask(value);
+        this.newTaskCtrl.setValue('');
+      }
+    }
   }
 
   addTask(title: string) {
     const newTask = {
       id: Date.now(),
       title,
-      completed: false,
-    };
-    this.tasks.update((tasks) => [...tasks, newTask]);
+      completed: false
+    }
+    this.tasks.update(prevState => [...prevState, newTask]);
   }
 
   deleteTask(index: number) {
-    this.tasks.update((tasks) => tasks.filter((task, position) => position !== index));
+    this.tasks.update(state => {
+      const updated = [...state];  // Hacemos una copia
+      updated.splice(index, 1);    // Mutamos la copia
+      return updated;              // La devolvemos
+    });
   }
 
   updateTask(index: number) {
-    this.tasks.update((tasks) => {
-      return tasks.map((task, position) => {
+    this.tasks.update(state => {
+      const updated = [...state];
+      const currentTask = updated[index];
+      updated[index] = {
+        ...currentTask,
+        completed: !currentTask.completed
+      };
+      return updated;
+    });
+  }
+  updateTaskEditingMode(index: number) {
+    this.tasks.update(prevState => {
+      return prevState.map((task, position) => {
         if (position === index) {
           return {
             ...task,
-            completed: !task.completed
+            editing: true
+          }
+        }
+        return {
+          ...task,
+          editing: false
+        };
+      })
+    });
+  }
+
+  updateTaskText(index: number, event: Event) {
+    const input = event.target as HTMLInputElement;
+    this.tasks.update(prevState => {
+      return prevState.map((task, position) => {
+        if (position === index) {
+          return {
+            ...task,
+            title: input.value,
+            editing: false
           }
         }
         return task;
       })
-    })
+    });
+  }
+
+  changeFilter(filter: 'all' | 'pending' | 'completed') {
+    this.filter.set(filter);
   }
 }
--- src/app/pages/labs/labs.ts
+++ src/app/pages/labs/labs.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, signal} from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
